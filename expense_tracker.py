c604cdd3b68890bcdf9261fe81beaab98b907273
Barakath Haseena <hasee300906@gmail.com>
2025-05-25 03:51:08 -0700

    Improve input handling and Customized Categories

--- expense_tracker.py
+++ expense_tracker.py
@@ -5,8 +5,8 @@ def load_data():
     try:
         with open("expenses.json", "r") as file:
             return json.load(file)
-    except FileNotFoundError:
-        return {"expenses":[]}
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {"last_id": 0, "expenses":[]}
 
 def save_data(data):
     with open("expenses.json", "w") as file:
@@ -36,21 +36,48 @@ def valid_date(date_input):
         print("Invalid Date Format. Please try again.")
         return None
 
-def add_expense(description, amount, date):
-    data = load_data()
-    expense_id = len(data["expenses"]) + 1
+def choose_category():
+    categories = ["Home", "Work", "Food", "Entertainment", "Custom"]
+    
+    print("\nChoose a category: ")
+    for i, item  in enumerate(categories, 1):
+        print(f"{i}. {item}")
 
-    expense = {
-    "id": expense_id,
+    while True:
+        try:
+            choice = input("Enter the number of your category choice: ")
+            if choice.isdigit() and 1 <= int(choice) <= len(categories):
+                choice = int(choice)
+                if choice == len(categories):
+                    custom_item = input("Enter your custom category: ").strip().title()
+                    return custom_item if custom_item else "General"
+                else:
+                    return categories[choice - 1]
+            else:
+                print("Invalid choice, please enter a valid number.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+def add_expense(description, amount, date, category):
+    data = load_data()
+
+    existing_ids = [exp["id"] for exp in data["expenses"] if "id" in exp]
+    new_id = max(existing_ids, default = 0) + 1
+
+    data["last_id"] = new_id
+
+    new_expense = {
+    "id": new_id,
     "description": description,
     "amount": amount,
-    "date": date
+    "date": date,
+    "category": category
     }
 
-    data["expenses"].append(expense)
+    data["expenses"].append(new_expense)
     save_data(data)
 
-    print(f"Expense added succesfully (ID: {expense_id})")
+    print(f"Expense added successfully (ID: {new_id})")
     input("\nPress Enter to return to the main menu...")
 
 
@@ -61,7 +88,7 @@ def view_expenses():
     else:
         print("Expenses List:")
         for expense in data["expenses"]:
-            print(f"ID: {expense['id']}, Description: {expense['description']}, Amount: {expense['amount']}, Date: {expense['date']} ")
+            print(f"ID: {expense['id']}, Description: {expense['description']}, Amount: {expense['amount']}, Date: {expense['date']}, Category: {expense.get('category', 'General')} ")
     input("\nPress Enter to return to the main menu...")
 
 def delete_expense():
@@ -82,7 +109,7 @@ def delete_expense():
     else:
        print(f"No expense found with the ID {delete_id}.")   
 
-    input("\nPress Enter to return to the main menu")
+    input("\nPress Enter to return to the main menu...")
 
 def update_expense():
     data = load_data()
@@ -96,12 +123,16 @@ def update_expense():
     for exp in data["expenses"]:
         if exp['id'] == update_id:
             expense_found =  True
-            print(f"Current details: Description: {exp['description']}, Amount: {exp['amount']}, Date: {exp['date']}")
+            print(f"Current details: Description: {exp['description']}, Amount: {exp['amount']}, Date: {exp['date']}, Category: {exp.get('category', 'None')}")
 
             new_description = input("Enter new description (if needed): ")            
-            
             if new_description:
                 exp['description'] = new_description
+
+            new_category = input("Enter new category (if needed): ").strip().title()
+            if new_category:
+                exp['category'] = new_category
+
             while True:
                 new_amount = input("Enter new amount(if needed): ")
                 if new_amount:
@@ -141,18 +172,26 @@ def main():
         choice = input("Enter Your Choice: ")
 
         if choice == "1":
-            description = input("Enter Expense Description: ")
+            description = input("Enter Expense Description: ").strip()
+            if not description:
+                print("Description cannot be empty.")
+                return
+
             while True:
                 amount = input("Enter Expense Amount: ")
                 amount = valid_amount(amount)
                 if amount is not None:
                     break
+
             while True:
                 date = input("Enter expense date (YYYY-MM-DD) or press Enter for today's date: ")
                 date = valid_date(date)
                 if date:
                     break
-            add_expense(description, amount, date)
+
+            category = choose_category()
+
+            add_expense(description, amount, date, category)
 
         elif choice == "2":
             view_expenses()    
